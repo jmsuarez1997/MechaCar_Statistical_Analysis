d8606df89df40783baf3cf57fb61b5c40ff528b0
Juana Suarez <jmsuarez1997@gmail.com>
2022-04-03 15:17:34 -0500

    adding imgs, Challenge, README

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-#Linear Regression to Predict MPG
+#1. Linear Regression to Predict MPG
 #Import libraries
 install.packages("tidyverse")
 library(tidyverse)
@@ -14,6 +14,16 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 #use summary to determine the p-value and the r-squared value for the linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar))
 
+
+# 2. Visualizations for the Trip Analysis
+#read in Suspension_Coil.csv
+Suspension <- read.csv(file='Resources/Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+head(Suspension)
+
+#create a total summary table for PSI - calculate mean, median, variance, standard deviation
+total_summary <- Suspension %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI),.groups = 'keep')
+
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Meadian = median(PSI), Variance = var(PSI), SD = sd(PSI),.groups = 'keep')
 
 
 
